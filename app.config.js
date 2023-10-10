c8e22b857a3e637d51746cc8c9e48db38ee612f2
ankushKun <ankush4singh@gmail.com>
2023-10-10 13:07:43 +0530

    sha added

---
+++ app.config.js
@@ -0,0 +1,44 @@
+export default {
+    "name": "Librihaven",
+    "slug": "LibFun",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "userInterfaceStyle": "light",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#e4cab0"
+    },
+    "assetBundlePatterns": [
+      "**/*"
+    ],
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/icon.png",
+        "backgroundColor": "#ffffff"
+      },
+      "package": "com.shamblet.librihaven",
+      "versionCode": 1,
+      "googleServicesFile": process.env.GOOGLE_SERVICES_JSON
+    },
+    "web": {
+      "favicon": "./assets/icon.png"
+    },
+    "extra": {
+      "eas": {
+        "projectId": "6853bfca-14e8-4b8a-b54f-5f29a154e79c"
+      }
+    },
+    "owner": "ankushkun",
+    "runtimeVersion": {
+      "policy": "appVersion"
+    },
+    "updates": {
+      "url": "https://u.expo.dev/6853bfca-14e8-4b8a-b54f-5f29a154e79c"
+    },
+    "plugins":["@react-native-google-signin/google-signin"]
+  }
